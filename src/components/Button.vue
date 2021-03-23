1f6d2708bc8ec0bca713957d381592a806b6518f
hsm0104 <sstyle940104@gmail.com>
2021-03-23 21:53:56 +0900

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button class='btn' :style="{backgroundColor: color}" >{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props:{
+        text: String,
+        color: String
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
